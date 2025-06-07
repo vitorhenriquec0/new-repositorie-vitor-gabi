1371e65ad1ba902f8c9e1fcf2f494085ca8fef0c
Molina <gabsoouza1@gmail.com>
2025-06-07 09:32:58 -0300

    testando cadastro dos produtos

---
+++ assets/js/produtos.js
@@ -0,0 +1,37 @@
+//firebase-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeRFLRmFhTujKWZuW2UndQj_jqu4_JJSU",
+  authDomain: "new-repositore-vitor-gabi.firebaseapp.com",
+  projectId: "new-repositore-vitor-gabi",
+  storageBucket: "new-repositore-vitor-gabi.appspot.com", 
+  messagingSenderId: "744773116089",
+  appId: "1:744773116089:web:f82dcb394e29394c84bb68"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//
+
+const form = document.getElementById('produtos');
+form.addEventListener('submit', async (e) =>{
+    e.preventDefault();
+
+const nome = form.nome.value;
+const preco = parseFloat(form.preco.value);
+
+try {
+    await addDoc(collection(db, "produtos"),{
+     nome,
+     preco
+    });
+    alert("Produto cadastrado com sucesso.");
+    form.reset();
+}catch (error) {
+    console.error ("Erro ao cadastrar produto", error);
+    alert("erro ao cadastrar");
+}
+})
